5a85b0ee1ccd001dc431e96ea11b2c3cde9827b5
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-07-20 16:23:39 +0900

    Book Collection View 기본틀 구현

---
+++ Book/BookCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BookCollectionViewCell.swift
+//  Shopping
+//
+//  Created by 신승아 on 2022/07/20.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var bookImage: UIImageView!
+    @IBOutlet weak var bookTitle: UILabel!
+    @IBOutlet weak var bookRate: UILabel!
+    
+}
